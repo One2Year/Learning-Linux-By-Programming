4a5396e3b7e3efe4a6fd0532eaca83bfdda73329
One2Year <793150526@qq.com>
2020-11-25 00:47:30 -0800

    Update C3 ls1 ls2

---
+++ Chapter3-ls/c3.sh
@@ -0,0 +1,6 @@
+ls
+ls -a   #show files begin with "."
+ls -lu  
+ls -s
+ls -t
+ls -F
---
+++ Chapter3-ls/fileinfo.c
@@ -0,0 +1,32 @@
+/* fileinfo.c - use stat() to obtain and print file properties 
+ *            - some members are just numbers...
+ */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main (int ac, char *av[]) {
+    //buffer for file info
+    struct stat info;
+
+    if (ac > 1) {
+        if (stat(av[1], &info) != -1) {
+            show_stat_info(av[1], &info);
+            return 0;
+        } else {
+            perror(av[1]);
+        }
+    }
+    return 1;
+}
+
+//display some info from stat in a name = value format
+show_stat_info(char *fname, struct stat *buf) {
+    printf("    mode : %o\n", buf->st_mode);
+    printf("   links : %d\n", buf->st_nlink);
+    printf("    user : %d\n", buf->st_uid);
+    printf("   group : %d\n", buf->st_gid);
+    printf("    size : %d\n", buf->st_size);
+    printf(" modtime : %d\n", buf->st_mtime);
+    printf("    name : %d\n", fname);
+}
