bf55049ea7b8c9f79e00be49cac0d55b1ec13a1b
小米-刘相廷 <793150526@qq.com>
2021-06-07 00:10:21 +0800

    Update CH11
    popendemo.c popen_ex3.c

---
+++ Chapter11-server-socket/popen_ex3.c
@@ -0,0 +1,18 @@
+/**
+ * popen_ex3.c
+ *      shows how to use popen to write to a process that
+ *      reads from stdin. This program writes email to
+ *      two users. Note how easy it is to use fprintf
+ *      to format the data to send.
+*/
+
+#include <stdio.h>
+
+main() {
+    FILE *fp;
+
+    fp = popen("mail admin backup", "w");
+    fprintf(fp, "Error with backup!!\n");
+    // pclose调用了wait函数来等待进程的结束
+    pclose(fp);
+}
